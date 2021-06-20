8970ae7df524c232a781063a05bb5b95a0c5d4b3
jukanntenn <75071550+jukanntenn@users.noreply.github.com>
2021-06-20 23:59:04 +0800

    Set CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,9 @@ from tencentcos_storage.storage import TencentCOSStorage
 def storage():
     return TencentCOSStorage(
         bucket="test-bucket",
-        config={"Region": "region", "SecretId": "********", "SecretKey": "********"},
+        config={
+            "Region": "region",
+            "SecretId": "********",
+            "SecretKey": "********",
+        },
     )
